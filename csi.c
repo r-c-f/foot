ad2539af4430751c5fceafc635a0b0a45cc438c0
Daniel Eklöf <daniel@ekloef.se>
2021-11-16 16:37:10 +0100

    csi: kitty: s/mode/flags in debug logs

--- csi.c
+++ csi.c
@@ -1558,7 +1558,7 @@ csi_dispatch(struct terminal *term, uint8_t final)
             grid->kitty_kbd.idx = idx;
 
             xassert(grid->kitty_kbd.idx < ALEN(grid->kitty_kbd.flags));
-            LOG_DBG("kitty kbd: pushed new mode: 0x%03x", flags);
+            LOG_DBG("kitty kbd: pushed new flags: 0x%03x", flags);
             break;
         }
 
@@ -1609,7 +1609,7 @@ csi_dispatch(struct terminal *term, uint8_t final)
             grid->kitty_kbd.idx = idx;
             xassert(grid->kitty_kbd.idx < ALEN(grid->kitty_kbd.flags));
 
-            LOG_DBG("kitty kbd: mode after pop: 0x%03x",
+            LOG_DBG("kitty kbd: flags after pop: 0x%03x",
                     term->grid->kitty_kbd.flags[idx]);
             break;
         }
